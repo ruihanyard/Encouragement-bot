b6943c08efae06451255884385395a15da553ce6
quaKe3000 <52991746+quaKe3000@users.noreply.github.com>
2020-10-20 11:58:26 +0800

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -13,20 +13,42 @@ while True:
     
     if each_word == "sad":
       feelings_list.append("sad")
-      encouragement_list.append("tomorrow will be a better day")
-      counter += 1
-    if each_word == "happy":
-      feelings_list.append("happy")
-      encouragement_list.append("to keep smiling")
-      counter += 1
-    if each_word == "tired":
-      feelings_list.append("tired")
-      encouragement_list.append("you are stronger than you think")
+      encouragement_list.append("Tomorrow will be a better day!")
       counter += 1
       
-if each_word == "anxious":
+    if each_word == "happy":
+      feelings_list.append("happy")
+      encouragement_list.append("Keep smiling!")
+      counter += 1
+      
+    if each_word == "tired":
+      feelings_list.append("tired")
+      encouragement_list.append("You are stronger than you think!")
+      counter += 1
+      
+   if each_word == "anxious":
       feelings_list.append("anxious")
       encouragement_list.append("take a deep breath")
+      counter += 1
+      
+   if each_word == "stressed":
+      feelings_list.append("stressed")
+      encouragement_list.append("take a deep breath")
+      counter += 1
+      
+   if each_word == "frustrated":
+      feelings_list.append("frustrated")
+      encouragement_list.append("take a deep breath and calm your mind")
+      counter += 1
+      
+   if each_word == "disappointed":
+      feelings_list.append("disappointed")
+      encouragement_list.append("You are amazing, keep on fighting!")
+      counter += 1
+      
+   if each_word == "embarrassed":
+      feelings_list.append("embarrassed")
+      encouragement_list.append("Nothing about you is shameful, you are AMAZING!")
       counter += 1
       
   if counter == 0:
